fc4e8cb177960fe5049ed7bb09b8c33c538aae07
MattheusAlfaia <83054761+MattheusAlfaia@users.noreply.github.com>
2023-04-14 12:33:25 -0400

    Add files via upload

---
+++ src/components/UltimosLancamentos/index.jsx
@@ -0,0 +1,41 @@
+import { livros } from './data'
+import styled from 'styled-components'
+import Titulo from '../Titulo'
+import CardRecomenda from '../CardRecomenda'
+import imagemLivro from '../../imagens/livro2.png'
+
+const UltimosLancamentosContainer = styled.section`
+    background-color: #EBECEE;
+    padding-bottom: 20px;
+    display: flex;
+    flex-direction: column;
+`
+
+const NovosLivrosContainer = styled.div`
+    margin-top: 30px;
+    display: flex;
+    width: 100%;
+    justify-content: center;
+    cursor: pointer;
+`
+
+function UltimosLancamentos() {
+    return (
+        <UltimosLancamentosContainer>
+            <Titulo cor="#EB9B00" tamanhoFonte="36px" alinhamento="center">ÚLTIMOS LANCAMENTOS</Titulo>
+            <NovosLivrosContainer>
+                { livros.map( (livro) => (
+                    <img src={livro.src} alt={livro.titulo} />
+                ) ) }
+            </NovosLivrosContainer>
+            <CardRecomenda
+                titulo="Talvez você se interesse por"
+                subtitulo="Angular 11"
+                descricao="Construindo uma aplicação com a plataforma Google"
+                img={imagemLivro}
+            />
+        </UltimosLancamentosContainer>
+    )
+}
+
+export default UltimosLancamentos
